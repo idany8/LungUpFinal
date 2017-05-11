fbc32bf01eeb5e6b6b6b20e1cd20418981e13310
idany8 <idanynv@gmail.com>
2017-05-11 06:35:53 +0300

    exercise schedule layout

---
+++ app/src/main/java/com/example/idan/lungupfinal/ExerciseSchedule.java
@@ -0,0 +1,117 @@
+package com.example.idan.lungupfinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ExerciseSchedule extends AppCompatActivity {
+    String[] items;
+
+    ArrayList<String> listItems;
+
+    ArrayAdapter<String> adapter;
+
+    ListView listView;
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_schedule);
+
+        listView=(ListView)findViewById(R.id.set_schedule_listview);
+
+        editText=(EditText)findViewById(R.id.set_schedule_txtsearch);
+
+        initList();
+
+        editText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int
+                    after) {
+
+
+
+            }
+
+
+
+            @Override
+
+            public void onTextChanged(CharSequence s, int start, int before, int
+                    count) {
+
+                if(s.toString().equals("")){
+
+                    // reset listview
+
+                    initList();
+
+                }
+
+                else{
+
+                    // perform search
+
+                    searchItem(s.toString());
+
+                }
+
+            }
+
+
+
+            @Override
+
+            public void afterTextChanged(Editable s) {
+
+
+
+            }
+
+        });
+
+    }
+
+
+
+    public void searchItem(String textToSearch){
+
+        for(String item:items){
+
+            if(!item.contains(textToSearch)){
+
+                listItems.remove(item);
+
+            }
+
+        }
+
+        adapter.notifyDataSetChanged();
+
+    }
+
+    public void initList(){
+
+        items=new String[]{"Canada","China","Japan","USA"};
+
+        listItems=new ArrayList<>(Arrays.asList(items));
+
+        adapter=new ArrayAdapter<String>(this,
+                R.layout.list_item, R.id.txtitem, listItems);
+
+        listView.setAdapter(adapter);
+
+    }
+
+}
